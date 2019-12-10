1d34e740aa5d597eab8be0ccb0031f68c29d1c0f
Thiago Tahara <thiagoftahara@gmail.com>
2019-12-10 18:56:26 -0300

    Using PropTypes
    Demo how to use PropTypes

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
+import CourseList from "./CourseList";
+
 export default function CoursesPage() {
   const [courses, setCourses] = useState([]);
 
@@ -11,26 +13,7 @@ export default function CoursesPage() {
   return (
     <>
       <h2>Courses</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author ID</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {courses.map(course => {
-            return (
-              <tr key={course.id}>
-                <td>{course.title}</td>
-                <td>{course.authorId}</td>
-                <td>{course.category}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <CourseList courses={courses} />
     </>
   );
 }
