adcff42b38b426e888b626ebefc21c4ee919a91b
Thiago Tahara <thiagoftahara@gmail.com>
2019-12-19 19:03:40 -0300

    Implementing TextInput component

---
+++ src/components/common/TextInput.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function TextInput(props) {
+  let wrapperClass = "form-group";
+  //   When you declare:
+  //   TextInput.defaultProps = {
+  //     error: ""
+  //   }
+  //   you certainly will have that prop
+  //   so you don't have to check this out
+  if (/*props.error && */ props.error.length > 0) {
+    wrapperClass += " has-error";
+  }
+
+  return (
+    <div className="wrapperClass">
+      <label htmlFor={props.id}>{props.label}</label>
+      <div className="field">
+        <input
+          id={props.id}
+          type="text"
+          onChange={props.onChange}
+          name="title"
+          className="form-control"
+          value={props.value}
+        />
+      </div>
+      {props.err && <div className="alert alert-danger">{props.error}</div>}
+      {/* The && operator like this executes the block on the right when the left is true */}
+    </div>
+  );
+}
+
+TextInput.protoTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+  error: PropTypes.string
+};
+
+TextInput.defaultProps = {
+  error: ""
+};
